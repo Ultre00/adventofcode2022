ab08f156d001171de4a049a56917b4acc9fe97af
Kevin Henzen <45797587+Ultre00@users.noreply.github.com>
2022-12-04 08:38:42 +0100

    4

---
+++ 4/index.js
@@ -0,0 +1,28 @@
+import { getInput } from "../shared.js";
+
+const raw = getInput("4/input.txt");
+const data = raw
+  .split("\r\n")
+  .map((m) => m.split(",").map((n) => n.split("-").map((o) => +o)));
+
+const isInside = (a, b) => a[0] <= b[0] && a[1] >= b[1];
+const isOverlapping = (a, b) =>
+  (a[0] >= b[0] && a[0] <= b[1]) || (a[1] >= b[0] && a[1] <= b[1]);
+
+//part 1
+const p1 = data.reduce(
+  (res, cur) =>
+    isInside(cur[0], cur[1]) || isInside(cur[1], cur[0]) ? res + 1 : res,
+  0
+);
+console.log(p1);
+
+//part2
+const p2 = data.reduce(
+  (res, cur) =>
+    isOverlapping(cur[0], cur[1]) || isOverlapping(cur[1], cur[0])
+      ? res + 1
+      : res,
+  0
+);
+console.log(p2);
